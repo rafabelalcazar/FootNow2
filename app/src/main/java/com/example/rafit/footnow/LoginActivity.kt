5401d1f9ebc33052c11f70130503697d784a73e9
rafabelalcazar <38873104+rafabelalcazar@users.noreply.github.com>
2018-12-23 17:22:18 -0500

    Se crea el item_layou_CardView de Cancha
    Se crea el item_layout para contener un cardview donde se van a mostrar las canchas

--- app/src/main/java/com/example/rafit/footnow/LoginActivity.kt
+++ app/src/main/java/com/example/rafit/footnow/LoginActivity.kt
@@ -2,11 +2,21 @@ package com.example.rafit.footnow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.startActivity
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+            startActivity<MainActivity>()
+        }
     }
+
+
+
+
 }
--- app/src/main/java/com/example/rafit/footnow/MainActivity.kt
+++ app/src/main/java/com/example/rafit/footnow/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.rafit.footnow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
+
+
+        txtTest.setOnClickListener{
+            startActivity<LoginActivity>()
+
+        }
+
+
     }
 }
